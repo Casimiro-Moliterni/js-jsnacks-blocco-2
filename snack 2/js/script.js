534837985e39f19ec8eff49496793b39496e04a7
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-03-12 16:50:18 +0100

    add : prompt with question for user

---
+++ snack 2/js/script.js
@@ -0,0 +1,13 @@
+// - L’utente inserisce due parole in successione, con due prompt.
+// Il software stampa prima la parola più corta, poi la parola più lunga.
+
+
+
+// prima creare array vuoto 
+const textArray = [];
+// chiedere all'utente due parole con due rpompt diversi 
+const userFirsText = prompt('scrivi una parola');
+console.log(userFirsText)
+// calcolare la lunghezza delle parole 
+
+// poi stampare in console prima la parola più corta e poi quella più lunga 
