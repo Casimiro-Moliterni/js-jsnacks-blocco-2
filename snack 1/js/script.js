221b0621b918d03d07574f51eae970c8354a721e
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-03-12 16:36:01 +0100

    add : prompt number user question

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -1,1 +1,10 @@
-alert('test');
+// - L'utente inserisce un numero nel prompt, se è pari stampa il numero,
+// se è dispari stampa il numero successivo
+
+
+// scrivere prompt inserisci un numero 
+
+const userNumber = parseInt(prompt('Scrivi un numero'));
+console.log(userNumber)
+
+// se il numero è pari stampalo altrimenti stampa il numero successivo 
